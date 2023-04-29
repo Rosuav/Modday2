890669ff3b3f17ad15fa5aa6d5d637a0c194ed4d
Chris Angelico <rosuav@gmail.com>
2023-04-30 00:56:00 +1000

    Show your orientation on demand

--- mod.lua
+++ mod.lua
@@ -38,8 +38,46 @@ function(self, from_pos, direction, dmg_mul, shoot_player, spread_mul, autohit_m
 end)
 --]]
 
+-- Precomputed sines of every sixteenth of a circle, first quadrant:
+local sixteenths = {0.19509032201612825, 0.5555702330196022, 0.8314696123025452, 0.9807852804032304}
+local labels = {"N", "NNE", "NE", "ENE"}
+
 Hooks:PreHook(PlayerStandard, "_check_action_reload", "auto_reload",
 function(self, t, input)
+	-- Where am I? If you're pressing both Reload and Interact at once, you're lost, and need to
+	-- be told where you are. Okay, that's not exactly what I do, but I say where you're facing.
+	if input.btn_reload_press and input.btn_interact_press then
+		-- self._cam_fwd_flat is (x, y, 0) representing a unit vector (ie x*x+y*y will always
+		-- equal 1). We want the angle of that vector, then divide that by 1/16th of a circle
+		-- and round to the nearest cardinal position. However, since trignometric functions
+		-- are expensive (and cardinals even more so, whether we're talking about birds or
+		-- clergy), it's better to use the rectangular dimensions directly.
+		-- Step 1: Precompute one quadrant's coordinates. (It doesn't matter whether we use
+		-- sin or cos here, just determines which way around we look at it.)
+		-- [sin(x / 8 * pi) for x in range(5)]
+		-- Notionally, this gives us our target locations. However, it's more useful to have
+		-- the boundaries between them, which we can use in comparisons. So shift around a bit.
+		-- [sin((x / 8 + 1/16) * pi) for x in range(4)]
+		-- Step 2: Find which octant we're in - NE, NW, SW, SE - and adjust accordingly.
+		local x = math.abs(self._cam_fwd_flat.x)
+		local label = "E" -- The highest X values correspond to being nearly out of the quadrant.
+		for i, pos in pairs(sixteenths) do
+			if x < pos then
+				label = labels[i]
+				break
+			end
+		end
+		-- Cool. That's fine if we're in the NE quadrant. What if we're not?
+		if self._cam_fwd_flat.x < 0 then
+			label = string.gsub(label, "E", "W")
+		end
+		if self._cam_fwd_flat.y < 0 then
+			label = string.gsub(label, "N", "S")
+		end
+		say("You are facing: " .. label)
+	end
+
+	-- Autoreload: if you need to reload and can reload, assume you're pressing the reload key
 	local weapon_unit = managers.player:equipped_weapon_unit()
 	if weapon_unit then
 		local clip = weapon_unit:base()._ammo_remaining_in_clip
